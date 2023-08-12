cc1bcf754ea524bf5c54bd5454bd6fdd399dddf3
Chidinma-onyinye <chidinmam67@gmail.com>
2023-08-12 03:18:22 -0700

    0-positive_or_negative.c

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
+ * main - Determine if a number is positive, negative or zero
  *
  * Return: Always 0 (Success)
  */
@@ -14,16 +14,13 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
 {
-printf("%d is positive\n", n);
+	printf("%d is positive\n", n);
 }
-if (n < 0)
+else if (n == 0)
 {
-printf("%d is negative\n", n);
-}
-if (n == 0)
+	printf("%d is zero\n", n); }
+else
 {
-printf("%d is zero\n", n);
+	printf("%d is negative\n", n);
 }
-return (0);
-}
-
+return (0); }
